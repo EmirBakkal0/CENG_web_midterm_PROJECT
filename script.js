4a13bec30ec7b3b63bf45a399a99bf5ad79a3d11
EmirBakkal0 <ebakkal2@gmail.com>
2024-11-24 22:11:49 +0300

    added add student, course funcs

--- script.js
+++ script.js
@@ -9,29 +9,53 @@ function router(divID){
 
 
 class Course{
-    constructor(courseName,gradeScale){
+    constructor(courseName,gradeScale,students){
         this.courseName=courseName
         this.gradeScale=gradeScale
+        this.students=students
 
+    }
+
+    addStudent(student,midterm,final){
+        this.students.push({"studentObject":student,"midterm":midterm,"final":final})
     }
 
 }
 
-let studentid=0
+
 class Student{
-    constructor(name){
-        this.id=studentid++
+    constructor(name,studentid,courses){
+        this.studentid=studentid;
         this.name=name;
+        this.courses=courses
+    }
+
+    edit(name,id){
+        this.name=name;
+        this.studentid=id;
+    }
+    addCourse(course,midterm,final){
+        this.courses.push({"courseObject":course,"midterm":midterm,"final":final})
     }
     
 }
 
 
-const courses=[
-    new Course("Database","7")
-]
+const coursesJson= JSON.parse( localStorage.getItem("courses")) ?? []
+// get courses from local storage if exists or create an empty array
+const courses= coursesJson.map((course) => new Course(course.courseName,course.gradeScale,course.students))
+console.log(courses)
 
 
+// const courses=[
+//     new Course("Database","7")
+// ]
+
+const studentsJson= JSON.parse(localStorage.getItem("students")) ?? []
+const students= studentsJson.map((student) => new Student(student.name,student.studentid,student.courses))
+/*const students=[
+    new Student("Emir Bakkal","220709004")
+]*/
 
 
 const courseForm= document.querySelector("#courseForm")
@@ -43,6 +67,7 @@ courseForm.addEventListener("submit", (event) =>{
     console.log(courseName,gradeScale);
     courses.push(new Course(courseName,gradeScale))
     updateCourseTable()
+
     event.target.reset();
 
 })
@@ -61,6 +86,8 @@ function updateCourseTable(){
             </td>
          </tr>`
     }
+    const courseJSON=JSON.stringify(courses)
+    localStorage.setItem("courses",courseJSON)
 
 }
 
@@ -70,4 +97,200 @@ function delCourse(index){
     
 }
 
+const studentForm= document.querySelector("#studentForm")
+studentForm.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    const studentName= document.querySelector("#studentName").value
+    const studentid= document.querySelector("#studentid").value
+    students.push(new Student(studentName,studentid))
+    updateStudentTable()
+    event.target.reset();
+
+})
+
+function updateStudentTable(){
+    console.log(students)
+    const tableBody=document.querySelector("#studentTableBody")
+    tableBody.innerHTML = ""
+
+    students.forEach((student,index) =>{
+      const row=document.createElement("tr");
+      row.innerHTML=`
+      
+      <td>${student.name}</td>
+      <td>${student.studentid}</td>
+            
+      <td>
+          <button onclick="editStudent(${index})"> Edit Student ${index} </button>
+          <button onclick="delStudent(${index})" > Delete Student </button>
+      </td>
+      `;
+        tableBody.appendChild(row);
+
+    })
+
+    localStorage.setItem("students",JSON.stringify(students))
+
+    /*for (let i=0; i< students.length; i++)  {
+        tableBody.innerHTML+=
+        `<tr>
+            <td>${students[i].name}</td>
+            <td>${students[i].studentid}</td>
+            
+            <td>
+                <button onclick="editStudent(${i})" > Edit Student ${i} </button>
+                <button onclick="delStudent(${i})" > Delete Student </button>
+            </td>
+         </tr>`
+    }*/
+
+}
+function delStudent(index){
+    students.splice(index, 1);
+    updateStudentTable();
+
+}
+
+/*function editStudent(index) {
+    console.log(index)
+    const dialog = document.querySelector("#studentEditDialog");
+    dialog.showModal()
+    const closeButton= document.querySelector("#closeStudentDialog")
+
+    const formName= document.querySelector("#studentNameDialog")
+    formName.value= students[index].name
+    const formId= document.querySelector("#studentidDialog")
+    formId.value= students[index].studentid
+
+    const formButton = document.querySelector("#studentSubmitButton")
+    formButton.addEventListener("click",(event) =>{
+        event.preventDefault();
+        students[index].name=formName.value;
+        students[index].studentid=formId.value;
+        updateStudentTable()
+        dialog.close()
+    })
+
+
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+
+    });
+}*/
+function editStudent(index) {
+    const dialog = document.querySelector("#studentEditDialog");
+    dialog.showModal()
+    const closeButton= document.querySelector("#closeStudentDialog")
+
+    const formName= document.querySelector("#studentNameDialog")
+    formName.value= students[index].name
+    const formId= document.querySelector("#studentidDialog")
+    const id= students[index].studentid
+    formId.value=id
+
+    const formButton = document.querySelector("#studentSubmitButton")
+    formButton.addEventListener("click",(event) =>{
+        event.preventDefault();
+        console.log(id)
+        const selectedStudent=students.find((student) => student.studentid===id)
+        console.log(selectedStudent)
+
+        selectedStudent.edit(formName.value,formId.value)
+
+
+        updateStudentTable()
+        dialog.close()
+    })
+
+
+    closeButton.addEventListener("click", () => {
+        dialog.close();
+
+    });
+}
+
+
+function updateCombinedDropdowns() {
+    const courseSelect = document.querySelector("#courseSelect");
+    const studentSelect = document.querySelector("#studentSelect");
+    students.forEach(student =>{
+        const option = `<option value="${student.name}">${student.name}</option>`;
+        studentSelect.innerHTML += option;
+    })
+    // const statsCourseSelect = document.getElementById("statsCourseSelect");
+    // courseSelect.innerHTML = `<option value="">Select a Course</option>`;
+    courses.forEach(course => {
+        const option = `<option value="${course.courseName}">${course.courseName}</option>`;
+        courseSelect.innerHTML += option;
+        // statsCourseSelect.innerHTML += option;
+    });
+}
+
+const combinedForm= document.querySelector("#editCourseForm")
+
+combinedForm.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const course= document.querySelector("#courseSelect").value
+    console.log(course)
+    const student= document.querySelector("#studentSelect").value
+    const midterm= document.querySelector("#midterm").value
+    const final= document.querySelector("#final").value
+
+    let chosenCourse=null
+    let chosenStudent=null
+    for (let i=0; i<courses.length; i++){
+        if (courses[i].courseName===course){
+            chosenCourse=courses[i]
+            console.log("chosen course:"+chosenCourse)
+        }
+    }
+    for (let i=0; i<students.length; i++){
+        if (students[i].name===student){
+            chosenStudent=students[i]
+            console.log("chosen student:"+chosenStudent)
+        }
+    }
+
+    chosenCourse.addStudent(chosenStudent,midterm,final)
+    //chosenStudent.addCourse(chosenCourse,midterm,final)
+
+    updateCombinedTable()
+     event.target.reset();
+
+})
+
+function updateCombinedTable(){
+    const tableBody = document.querySelector("#coursesAndStudentsTableBody")
+    tableBody.innerHTML = ""
+
+    courses.forEach((course,index) =>{
+        course.students.forEach((studentObj,stuIndex ) => {
+            const row=document.createElement("tr");
+            row.innerHTML=`
+                <td>${course.courseName}</td>
+                <td>${studentObj.studentObject.name}</td>
+                <td>${studentObj.studentObject.studentid}</td>
+                <td>${studentObj.midterm} </td>
+                <td>${studentObj.final} </td>
+                <td> grade</td>
+                 `
+            tableBody.appendChild(row);
+        })
+
+
+
+
+    })
+
+    localStorage.setItem("students",JSON.stringify(students))
+    localStorage.setItem("courses",JSON.stringify(courses))
+
+}
+
+
+
 updateCourseTable()
+updateStudentTable()
+updateCombinedDropdowns()
+updateCombinedTable()
