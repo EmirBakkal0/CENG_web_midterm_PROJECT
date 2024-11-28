0189b5afbc23ea6b06f61c227df5c5c6b43d4a8b
u220709004 <ebakkal2@gmail.com>
2024-11-28 12:09:31 +0300

    added comments to codes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 if (window.location.hash === ""){
-    window.location.hash="courses"
+    window.location.hash="courses" // this sets the default section
 }
-function router(divID){
+function router(divID){ 
+    /// this is used to navigate between different sections 
 
     
     if (divID==="editCourse"){
@@ -29,6 +30,7 @@ function router(divID){
 
 
 class Course{
+    //course class
     constructor(courseName,gradeScale,students){
         this.courseName=courseName
         this.gradeScale=gradeScale
@@ -51,6 +53,7 @@ class Course{
 
 
 class Student{
+    //student class
     constructor(name,studentid,courses){
         this.studentid=studentid;
         this.name=name;
@@ -94,35 +97,39 @@ class Student{
     
 }
 
-if (!localStorage.getItem("courses")){
+if (!localStorage.getItem("courses")){  // if the localstorage is empty than we fetch from local json file 
     fetch("courses.json" )
         .then(response => response.json())
         .then(data => localStorage.setItem("courses",JSON.stringify(data)))
 }
 
 const coursesJson= JSON.parse( localStorage.getItem("courses")) ?? []
-// get courses from local storage if exists or fetch it from local json file
+// get courses from local storage if exists or create empty array
+
+// map the courses from local storage to Course Class objs
 const courses= coursesJson.map((course) => new Course(course.courseName,course.gradeScale,course.students))
-console.log(courses)
 
 
 
-if (!localStorage.getItem("students")){
+if (!localStorage.getItem("students")){ // if the localstorage is empty than we fetch from local json file 
     fetch("students.json" )
         .then(response => response.json())
         .then(data => localStorage.setItem("students",JSON.stringify(data)))
 }
+// get students from local storage if exists or create empty array
+
 const studentsJson= JSON.parse(localStorage.getItem("students")) ?? []
+// map the courses from local storage to Student Class objs
 
 const students= studentsJson.map((student) => new Student(student.name,student.studentid,student.courses))
 
 
 const courseForm= document.querySelector("#courseForm")
-courseForm.addEventListener("submit", (event) =>{
+courseForm.addEventListener("submit", (event) =>{  //for handling addition of courses
     event.preventDefault();
 
     const courseName= document.querySelector("#courseName").value
-    if (courses.find((crs) => crs.courseName === courseName) ){
+    if (courses.find((crs) => crs.courseName === courseName) ){ // if the crs already exists
         alert("There's already a course with the same name..")
         return
     }
@@ -130,7 +137,7 @@ courseForm.addEventListener("submit", (event) =>{
     const gradeScale= document.querySelector("#gradeScale").value
 
 
-    courses.push(new Course(courseName,gradeScale,[]))
+    courses.push(new Course(courseName,gradeScale,[]))  // add the new course to array
     updateCourseTable()
 
     event.target.reset();
@@ -139,7 +146,8 @@ courseForm.addEventListener("submit", (event) =>{
 
 
 
-function updateCourseTable(){
+function updateCourseTable(){ 
+    /// populate the course table
     const tableBody=document.querySelector("#courseTableBody")
 
     tableBody.innerHTML = ""
@@ -155,10 +163,10 @@ function updateCourseTable(){
          </tr> `
     }
     const courseJSON=JSON.stringify(courses)
-    localStorage.setItem("courses",courseJSON)
+    localStorage.setItem("courses",courseJSON) // add to localStorage
 
 }
-function viewDetailsInCourse (crsName) {
+function viewDetailsInCourse (crsName) { // for redirecting to search section
     router('searchCourse')
 
     document.querySelector('#courseSearchSelection').value=crsName
@@ -170,28 +178,28 @@ function delCourse(index){
     updateCourseTable();
     
 }
-function editStudent(index) {
+function editStudent(index) {  // for handling the editStudent dialog
     const dialog = document.querySelector("#studentEditDialog");
-    dialog.showModal()
+    dialog.showModal() // open the modal
     const closeButton= document.querySelector("#closeStudentDialog")
 
     const formName= document.querySelector("#studentNameDialog")
-    formName.value= students[index].name
-    //const formId= document.querySelector("#studentidDialog")
+    formName.value= students[index].name  // populate the inputs  name value from table
+    
     const id= students[index].studentid
-    // formId.value=id
+   
 
     const formButton = document.querySelector("#studentSubmitButton")
-    formButton.addEventListener("click",(event) =>{
+    formButton.addEventListener("click",(event) =>{  // when submit gets clicked
         event.preventDefault();
-        console.log(id)
+        
         const selectedStudent=students.find((student) => student.studentid===id)
         console.log(selectedStudent)
 
         selectedStudent.editName(formName.value)
         alert("Student's name is updated to: "+formName.value)
         updateStudentTable()
-        window.location.reload();
+        window.location.reload();  //reloading page here because there s a glitch if you try to edit again without refleshing
 
         dialog.close()
     })
@@ -204,12 +212,12 @@ function editStudent(index) {
 }
 
 const studentForm= document.querySelector("#studentForm")
-studentForm.addEventListener("submit", (event) =>{
+studentForm.addEventListener("submit", (event) =>{  /// adding new students
     event.preventDefault();
 
     const studentName= document.querySelector("#studentName").value
     const studentID= document.querySelector("#studentid").value
-    if (findStudentById(studentID)){
+    if (findStudentById(studentID)){  // if the student already in db
         alert("A student with the same ID already exists..")
         return;
     }
@@ -220,6 +228,8 @@ studentForm.addEventListener("submit", (event) =>{
 })
 
 function updateStudentTable(){
+
+    /// populate student table
     console.log(students)
     const tableBody=document.querySelector("#studentTableBody")
     tableBody.innerHTML = ""
@@ -259,6 +269,7 @@ function findStudentById(id){
 
 
 function updateCombinedDropdowns() {
+    // for populating dropdowns 
     const courseSelect = document.querySelector("#courseSelect");
     const studentSelect = document.querySelector("#studentSelect");
     studentSelect.innerHTML=""
@@ -278,7 +289,7 @@ function updateCombinedDropdowns() {
 
 const combinedForm= document.querySelector("#editCourseForm")
 
-combinedForm.addEventListener("submit", (event) =>{
+combinedForm.addEventListener("submit", (event) =>{  // adding students in the courses
     event.preventDefault();
     const courseName= document.querySelector("#courseSelect").value
     console.log(courseName)
@@ -305,6 +316,7 @@ combinedForm.addEventListener("submit", (event) =>{
 
 
 function updateCombinedTable(){
+    //populate table
     const tableBody = document.querySelector("#coursesAndStudentsTableBody")
     tableBody.innerHTML = ""
 
@@ -316,6 +328,7 @@ function updateCombinedTable(){
 }
 
 function calcGradeLetter(midterm,final,gradescale){
+    //for calculating grade letter
     const total= 0.4*midterm + 0.6*final;
     if (gradescale === "10"){
         return total >= 90 ? "A" : total >= 80 ? "B" : total >= 70 ? "C" : total >= 60 ? "D" : "F";
@@ -329,6 +342,7 @@ function calcGradeLetter(midterm,final,gradescale){
 
 
 document.querySelector("#searchStudentForm").addEventListener("submit",(event) => {
+    // for searching students
     event.preventDefault()
     const searchedName= document.querySelector("#studentSearch").value;
 
@@ -344,9 +358,6 @@ document.querySelector("#searchStudentForm").addEventListener("submit",(event) =
         tableCaption.innerHTML="The results for "+searchedName+": Could not be found" ;
         return
     }
-
-
-
 
     studentResult.forEach((student) =>{
         const gpa = document.createElement("h2");
@@ -391,6 +402,8 @@ document.querySelector("#searchStudentForm").addEventListener("submit",(event) =
 
 
 function listCourses(courseList,tableBody,filter) {
+    ///for listing students and courses together
+
     const detailCheckbox = document.querySelector("#detailCheckbox")
     let passedCount=0
     let failedCount=0
@@ -418,7 +431,7 @@ function listCourses(courseList,tableBody,filter) {
             </thead>
             `
 
-            if (filter==="passed"){
+            if (filter==="passed"){  ///for passed filter
                 course.students.forEach((student, index) =>{
                     const gradeLetter = calcGradeLetter(student.midterm,student.final,course.gradeScale)
                     if (gradeLetter !=="F"){
@@ -447,7 +460,7 @@ function listCourses(courseList,tableBody,filter) {
 
                 })
             }
-            else if (filter==="showAll")
+            else if (filter==="showAll") ///for showAll filter
             course.students.forEach((student) =>{
 
                 const row=document.createElement("tr");
@@ -476,7 +489,7 @@ function listCourses(courseList,tableBody,filter) {
                 tableBody.appendChild(row);
             })
 
-            else if (filter==="failed"){
+            else if (filter==="failed"){///for failed students filter
                 course.students.forEach((student) =>{
                     const gradeLetter = calcGradeLetter(student.midterm,student.final,course.gradeScale)
                     if (gradeLetter ==="F"){
@@ -524,7 +537,7 @@ function listCourses(courseList,tableBody,filter) {
         }   
     })
 }
-function delScore(stuID,crsName){
+function delScore(stuID,crsName){ //delete a students score from a course
 
     const stu=findStudentById(stuID.toString())
     stu.delCourse(crsName)
@@ -533,7 +546,9 @@ function delScore(stuID,crsName){
     const table= document.querySelector("#searchedCourseTable tbody")
     table.innerHTML=""
 }
-function editScore(stuID,crsName,midtermScore,finalScore) {
+function editScore(stuID,crsName,midtermScore,finalScore) { 
+    //edit a students score from a course
+
     const dialog = document.querySelector("#scoreEditDialog");
     dialog.showModal()
     const closeButton= document.querySelector("#closeScoreDialog")
@@ -575,6 +590,7 @@ function editScore(stuID,crsName,midtermScore,finalScore) {
 
 
 function calcMeanOfCourse(course){
+    // for calculation of mean score
     // A:4 B:3 C:2 D:1 F:0
     let mean=0
     course.students.forEach((student) => {
@@ -588,6 +604,8 @@ function calcMeanOfCourse(course){
 
 
 const searchCourseFunc = (event,method) => {
+
+    /// used for searching or selecting a course 
 
     event.preventDefault()
     const tableCaption=document.querySelector("#searchedCourseTable caption")
@@ -630,6 +648,7 @@ const searchCourseFunc = (event,method) => {
 
 }
 function populateCourseSelection(){
+    // used for populating courses in the dropdown
     const courseSelect=document.querySelector("#courseSearchSelection")
     courseSelect.innerHTML=""
 
@@ -651,6 +670,6 @@ document.querySelector("#searchCourseForm2").addEventListener("submit",(event) =
 
 
 
-router(window.location.hash.replace("#",""));  // this is 
-updateCourseTable()
+router(window.location.hash.replace("#",""));  // this is to keep track of where the user was before refreshing
+updateCourseTable() // used for updating table in first load
 
